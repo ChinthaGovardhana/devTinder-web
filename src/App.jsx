c3222f111643b7a07d879311728ce5ac6d61cb86
ChinthaGovardhana <chinthagovardhana90@gmail.com>
2025-03-08 12:19:38 +0530

    Configured tailwind css and daisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import NavBar from "./components/NavBar"
 
 
 function App() {
@@ -5,8 +6,8 @@ function App() {
 
   return (
     <>
-      <h1>Hello world</h1>
-   
+    <NavBar/>
+      <h1 className="text-3xl font-bold underline mx">Hello world</h1>
     </>
   )
 }
